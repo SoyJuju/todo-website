e3106af3ac52b1ab003337b58fd5d78cc08abcad
SoyJuju <julianthant@gmail.com>
2023-09-24 20:51:06 +0630

    vercel integration

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ const PORT = process.env.VITE_PORT;
 connectDb();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ['https://todo-website-d98cjejfa-soyjuju.vercel.app'],
+    methods: ['POST', 'GET', 'DELETE', 'PUT'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use('/api/todo-list', todoRoutes);
