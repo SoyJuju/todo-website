c7dceaf81947fea885116520a7b19259b681877d
SoyJuju <julianthant@gmail.com>
2023-09-24 22:46:29 +0630

    not split

--- api/server.js
+++ api/server.js
@@ -8,8 +8,6 @@ dotenv.config();
 
 /*global process*/
 
-const PORT = process.env.VITE_PORT;
-
 connectDb();
 
 const app = express();
@@ -25,6 +23,10 @@ app.use(express.json());
 app.use('/api/todo-list', todoRoutes);
 app.use('/api/users', userRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+if (process.env.VITE_PORT) {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+  });
+}
+
+return app;
