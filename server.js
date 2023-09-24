578f11b70857b93cd83964815e7c5dd91944f744
SoyJuju <julianthant@gmail.com>
2023-09-24 18:34:22 +0630

    add npm

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import cors from 'cors';
 import todoRoutes from './router/todoRouter.js';
 import userRoutes from './router/userRouter.js';
 import connectDb from './config/dbConnection.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+/*global process*/
+
+const PORT = process.env.VITE_PORT;
 
 connectDb();
 
@@ -13,6 +19,6 @@ app.use(express.json());
 app.use('/api/todo-list', todoRoutes);
 app.use('/api/users', userRoutes);
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000.');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
